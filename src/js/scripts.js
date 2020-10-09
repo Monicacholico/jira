d3f5fb3b05edac61a2158c7a5a0377a56726f067
Monicacholico <monicachol@hotmail.com>
2020-10-08 21:41:36 -0500

    cloning

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,3 +8,43 @@ class Modal {
     }
 
 }
+
+
+import { ProjectList } from '../src/app/projectList.js';
+
+globalThis.DEFAULT_VALUE = 'MAX';
+
+class App {
+    static init() {
+        const activeProjectList = new ProjectList('active');
+        const finishedProjectsList = new ProjectList('finished');
+        activeProjectList.setSwitchHandlerFunction(
+            finishedProjectsList.addProject.bind(finishedProjectsList)
+            );
+        finishedProjectsList.setSwitchHandlerFunction(
+            activeProjectList.addProject.bind(activeProjectList)
+            );  
+        // this.startAnalytics();
+
+        // const someScript = document.createElement('script');
+        // someScript.textContent = 'alert("Hi there")';
+        // document.head.append(someScript);
+
+        const timerId = setTimeout(this.startAnalytics, 3000);
+        document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+            clearTimeout(timerId);
+
+        });
+
+    }
+
+
+        static startAnalytics() {
+            const analyticsScript = document.createElement('script');            
+            analyticsScript.src = 'scripts/utility/analytics.js';
+            analyticsScript.defer = true;
+            document.head.append(analyticsScript);
+    }
+}
+
+App.init();
