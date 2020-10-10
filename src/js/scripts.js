2eb2f79d2e125ef9869308ea8b7b207d0e3d8163
Monicacholico <monicachol@hotmail.com>
2020-10-09 23:56:53 -0500

    adding modal

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,50 +1,83 @@
 class Modal {
-    constructor() {
+    constructor(card) {
         this.createBtn = document.querySelector('.createBtn');
+        console.log(this.createBtn);
+        this.toDoBoard = document.querySelector('.to-do');
+        this.projectName = card.projectName;
+        this.text = card.text;
+        this.date = new Date();
+        this.asignee = ele.asignee;
+        this.initialCard();
     }
 
-    build() {
-        
+    initialCard() {
+        const listToDoCards = document.querySelector('.list-do-do');
+        const toDoCard = document.createElement('li');
+        listToDoCards.appendChild(toDoCard);
+        const liContent = `
+        <div class="card">
+            <div class="title-card">
+            <h2>${this.projectName}</h2>
+            </div>
+            <div class="task-card">
+            <p>${this.text}</p>
+            </div>
+            <div class="asignee-card">
+            <p>${this.asignee}</p>
+            </div>
+        </div>
+        `;
+
+        this.createBtn.addEventListener('click', this.initialCard.bind(this));
     }
+
+    build() {}
 
 }
 
-
-import { ProjectList } from '../src/app/projectList.js';
-
-globalThis.DEFAULT_VALUE = 'MAX';
-
-class App {
-    static init() {
-        const activeProjectList = new ProjectList('active');
-        const finishedProjectsList = new ProjectList('finished');
-        activeProjectList.setSwitchHandlerFunction(
-            finishedProjectsList.addProject.bind(finishedProjectsList)
-            );
-        finishedProjectsList.setSwitchHandlerFunction(
-            activeProjectList.addProject.bind(activeProjectList)
-            );  
-        // this.startAnalytics();
-
-        // const someScript = document.createElement('script');
-        // someScript.textContent = 'alert("Hi there")';
-        // document.head.append(someScript);
-
-        const timerId = setTimeout(this.startAnalytics, 3000);
-        document.getElementById('stop-analytics-btn').addEventListener('click', () => {
-            clearTimeout(timerId);
-
-        });
-
-    }
+const myModal = new Modal();
 
 
-        static startAnalytics() {
-            const analyticsScript = document.createElement('script');            
-            analyticsScript.src = 'scripts/utility/analytics.js';
-            analyticsScript.defer = true;
-            document.head.append(analyticsScript);
-    }
+function greeting() {
+    alert('hello world');
 }
+greeting();
 
-App.init();
+// import { ProjectList } from '../src/app/projectList.js';
+
+// globalThis.DEFAULT_VALUE = 'MAX';
+
+// class App {
+//     static init() {
+//         const activeProjectList = new ProjectList('active');
+//         const finishedProjectsList = new ProjectList('finished');
+//         activeProjectList.setSwitchHandlerFunction(
+//             finishedProjectsList.addProject.bind(finishedProjectsList)
+//             );
+//         finishedProjectsList.setSwitchHandlerFunction(
+//             activeProjectList.addProject.bind(activeProjectList)
+//             );  
+//         // this.startAnalytics();
+
+//         // const someScript = document.createElement('script');
+//         // someScript.textContent = 'alert("Hi there")';
+//         // document.head.append(someScript);
+
+//         const timerId = setTimeout(this.startAnalytics, 3000);
+//         document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+//             clearTimeout(timerId);
+
+//         });
+
+//     }
+
+
+//     //     static startAnalytics() {
+//     //         const analyticsScript = document.createElement('script');            
+//     //         analyticsScript.src = 'scripts/utility/analytics.js';
+//     //         analyticsScript.defer = true;
+//     //         document.head.append(analyticsScript);
+//     // }
+// }
+
+// App.init();
